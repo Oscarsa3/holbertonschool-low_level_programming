fe7c40ba5b8919b86e8d32b63f31a67b7a11851a
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-02-25 21:16:52 -0800

    Imprime la suma de los numeros del fibonacci son pares

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always: 0
+ */
+int main(void)
+{
+	int a = 0;
+	long int b = 1;
+	long int c = 2;
+	long int d = b;
+
+	while (b + c < 4000000)
+	{
+		c += b;
+		if (c % 2 == 0)
+			d += c;
+		b = c - b;
+	       a++;
+	}
+	printf("%ld\n", d);
+	return (0);
+}
