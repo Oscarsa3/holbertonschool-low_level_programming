a5f76fea1e6b4cebe5b9afb86a1fd623c693ecf5
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-02-25 21:34:02 -0800

    Imprime los 98 primeros numeros del fibonacci

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -10,10 +10,10 @@ int main(void)
 	int a;
 	unsigned long int b = 0, c = 1, d;
 
-	for (1 = 1; i <= 98; i++)
+	for (a = 1; a <= 98; a++)
 	{
 		d = b + c;
-		if (i != 98)
+		if (a != 98)
 		{
 			printf("%lu, ", d);
 		}
