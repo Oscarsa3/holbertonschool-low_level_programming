5ae070031c659797cfbfefdd1375326782747ff9
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-02-28 14:35:41 -0800

    Imprime enteros de un array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,17 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < (n - 1))
-	{
-		printf("%d, ", *a);
-			i++;
-			a++;
-	}
-	printf("%d\n", *a);
+	for (i = 0; i < n; i++)
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+
+	printf("\n");
 }
