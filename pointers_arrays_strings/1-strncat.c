42c57a589840e4738e605e0c3e19a169d4601c40
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-03-03 09:23:25 -0800

    concatena dos string hasta un derterminado tamaño

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - funcion
+ * @dest: puntero entrante
+ * @src: puntero entrente
+ * @n: variable
+ * Return: dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = 0;
+	int s = 0;
+	int co;
+
+	while (dest[d] != '\0')
+		d++;
+	while (src[s] != '\0')
+		s++;
+	for (co = 0; co < n; co++)
+	{
+		dest[d] = src[co];
+		d++;
+	}
+	return (dest);
+}
