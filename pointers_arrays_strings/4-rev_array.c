81ca8e1ef5af2b9212d3969d3e279da0f071fffd
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-03-03 14:13:59 -0800

    revierte una cadena de enteros

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - funcion
+ * @a: puntero
+ * @n: variable
+ */
+void reverse_array(int *a, int n)
+{
+	int b;
+	int i;
+	int c = n - 1;
+	
+	for (i = 0; i < c; i++)
+	{
+		b = a[i];
+		a[i] = a[c];
+		a[c] = b;
+		c--;
+	}
+}
