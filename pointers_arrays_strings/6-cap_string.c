84bf733f93fa4c7cf975159c8916bcfe28818fce
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-03-03 07:58:17 -0800

    Cambia por una mayuscula despues de un caracter especial

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - funcion
+ * @str: variable
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		if (*str == ' ' || *str == '\t' || *str == '\n' ||
+		*str == ',' || *str == ';' || *str == '.' || *str == '!' ||
+		*str == '?' || *str == '"' || *str == '(' || *str == ')' ||
+		*str == '{' || *str == '}')
+		{
+			i = 1;
+		}
+		else
+		{
+			if (i == 1)
+			{
+				if (*str >= 'a' && *str <= 'z')
+					*str = *str - 32;
+				i = 0;
+			}
+		}
+		str++;
+	}
+	return (str);
+}
