c19858a631448c25dca30930ee213a540ad2a74f
oscarsa3 <oscarorlandopsalinassilvestre@gmail.com>
2023-03-03 10:16:09 -0800

    copia un string en otro segun el maximo de bytes que se le indica

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - funcion
+ * @dest: destino
+ * @src: lo que se copiará
+ * @n: tamaño que se copiara
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int d = 0;
+	int s = 0;
+	int co;
+	
+	while (dest[d] != '\0')
+		d++;
+	while (src[s] != '\0')
+		s++;
+	for (co = 0; co < n && src[co] != '\0'; co++)
+	{
+		if (*dest == '\0' && * src == '\0')
+		{
+			return (dest);
+		}
+		dest[co] = src[co];
+	}
+	if (co < n)
+	{
+		dest[co] = '\0';
+	}
+	return (dest);
+}
