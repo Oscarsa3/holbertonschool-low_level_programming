2572ee0f925f71201b1eee687a9b9382bd397164
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-10 06:34:58 -0800

    Imprime el primer argumento o lo cambia

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - funcion
+ * @argc: cantidad de argumentos
+ * @argv: puntero al argumento
+ * Return: 0
+ *
+ */
+int main (int argc, char *argv[])
+{
+	argc = 0;
+
+	printf ("%s\n", argv[argc]);
+	return (0);
+}
