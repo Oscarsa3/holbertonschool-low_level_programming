16ae8bf24db1f71e2cbe1903c97fd87d250446e9
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-16 11:15:18 -0700

    Concatena dos string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int a, b;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (a = 0; a <= *s1; a++)
 	{
 	}
