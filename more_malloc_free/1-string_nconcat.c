5edfa20c08f9793e344aa45bd549a6593430a7e7
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-17 11:15:59 -0700

    Concatena dos string con un numero de bytes limite que nos indiquen

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - funcion para concatenar
+ * @s1: primer string
+ * @s2:segundo string
+ * @n: cantidad de bytes a concatenar
+ * Return: string concatenado
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int b = 0;
+	unsigned int c, d;
+	char *a;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = s2 = "";
+	}
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[b])
+	{
+		b++;
+	}
+	if (b > n)
+	{
+		b = n;
+	}
+	b += i;
+	a = malloc(sizeof(char *) * (b + 1));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < i; c++)
+	{
+		a[c] = s1[c];
+	}
+	for (d = 0; c < b; d++)
+	{
+		a[c] = s2[d];
+		c++;
+	}
+	c++;
+	a[c] = '\0';
+	return (a);
+
+}
