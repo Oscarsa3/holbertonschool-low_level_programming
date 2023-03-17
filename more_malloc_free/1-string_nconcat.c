85ac9d6a5439a6ed0f000733ec591f1ab31116f7
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-17 11:34:25 -0700

    Concatena dos string con un numero de bytes limite que nos indiquen

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	a = malloc(sizeof(char *) * (b + 1));
 	if (a == NULL)
 	{
-		return ("");
+		return (NULL);
+		free(a);
 	}
 	b = 0;
 	for (i = 0; s1[i]; i++)
