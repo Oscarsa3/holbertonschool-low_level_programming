d57a468fcbcdfde45fb67c4f6c618bf91255fab2
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-17 12:12:20 -0700

    Crea una matriz de enteros

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,19 +11,19 @@
  */
 int *array_range(int min, int max)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	int *a;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	a = malloc(sizeof(int) * (max - min) + sizeof(int));
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; min <= max; i++)
+	while (min <= max)
 	{
 		a[i] = min;
 		i++;
