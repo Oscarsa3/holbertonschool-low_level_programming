98e8793425f2ce8e2b784a7ef121ffd68b431e70
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-21 08:44:48 -0700

    Crea una estructura llamada dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Estructuras y typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT
+#define STRUCT
+/**
+ * struct dog - estructura
+ * @name: puntero a nombre
+ * @age: edad
+ * @owner: usuario
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
