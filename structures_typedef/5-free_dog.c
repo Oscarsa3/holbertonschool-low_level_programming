c561dd3ce10032e375ced6c0904fa87639c2a2a0
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-23 08:04:36 -0700

    Libera la estructura en memoria

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdli.h>
+#include "dog.h"
+/**
+ * void free_dog - funcion
+ * @d: puntero a la estructura
+ * Return: nada
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
