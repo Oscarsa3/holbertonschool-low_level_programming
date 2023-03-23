aa66e9900cc1d35029fe4a9b4c8ec80b19eaa309
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-23 07:00:28 -0700

    Imprime una estructura

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - funcion
+ * @d: puntero a la estructura
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+	if (d->age)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner)
+	{
+		printf("owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+	return;
+
+}
