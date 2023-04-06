bbd2e85d03c7168925c413a7e0a3a8a460768064
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-05 17:06:45 -0700

    Implementamos una funcion para imprimir los elementode dentro de las single linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * print_list - imprime listas anidadas
+  * @h: head que se envia
+  * Return: la cantidad de impresiones
+  */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
