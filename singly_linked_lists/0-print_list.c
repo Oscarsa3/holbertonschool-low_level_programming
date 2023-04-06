753c401d1b9ecbf6c5c03a50886d94a680add296
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-05 17:54:39 -0700

    Corrigiendo codigo para que solo imprima nil si hay nulo y no dos veces

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,14 +7,17 @@
 size_t print_list(const list_t *h)
 {
 	int i = 0;
-
+	
 	while (h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf ("[0] (nil)\n");
 		}
-		printf("[%u] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
