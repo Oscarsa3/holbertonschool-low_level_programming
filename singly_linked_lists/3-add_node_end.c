f75c295926c399ce680f1fa53617156593e5f035
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-07 19:42:20 -0700

    Agrega un nodo al final

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+  * add_node_end - agrega nodo al final
+  * @head: agrega
+  * @str: nuevo string
+  * Return: direccion del nuevo
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last;
+	list_t *end = malloc(sizeof(list_t));
+
+	end->str = strdup(str);
+	end->len = strlen(str);
+	end->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = end;
+	}
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = end;
+	}
+	return (end);
+}
