bd96f434bd29f59429f3c5cf03c6d04dd1a9c56c
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-14 15:11:24 -0700

    Actualizando para arreglar colisiones

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,37 +1,37 @@
 #include "hash_tables.h"
-/**
-  * hash_table_set - agrega un nuevo noda a un index
-  * @ht: tabla
-  * @key: llave
-  * @value: valor
-  * Return: 1
-  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	size_t index;
-	char *valor = strdup(value);
-
-	if (!ht | !key)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	/**
+	  * hash_table_set - agrega un nuevo noda a un index
+	  * @ht: tabla
+	  * @key: llave
+	  * @value: valor
+	  * Return: 1
+	  */
+	int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
-		ht->array[index] = malloc(sizeof(hash_node_t));
-		ht->array[index]->value = valor;
-		ht->array[index]->key = strdup(key);
-		ht->array[index]->next = NULL;
-	}
-	else
-	{
+		size_t index;
+		char *valor = strdup(value);
 
-		ht->array[index]->next = malloc(sizeof(hash_node_t));
-		if (!ht->array[index]->next)
+		if (!ht | !key)
 			return (0);
-		ht->array[index]->next->value = valor;
-		if (!ht->array[index]->next->value)
-			return (0);
-		ht->array[index]->next->key = (char *)key;
-		ht->array[index]->next->next = ht->array[index];
+		index = key_index((unsigned char *)key, ht->size);
+		if (ht->array[index] == NULL)
+		{
+			ht->array[index] = malloc(sizeof(hash_node_t));
+			ht->array[index]->value = valor;
+			ht->array[index]->key = strdup(key);
+			ht->array[index]->next = NULL;
+		}
+		else
+		{
+
+			ht->array[index]->next = malloc(sizeof(hash_node_t));
+			if (!ht->array[index]->next)
+				return (0);
+			ht->array[index]->next->value = valor;
+			if (!ht->array[index]->next->value)
+				return (0);
+			ht->array[index]->next->key = (char *)key;
+		ht->array[index]->next->next = ht->array[index]->next;
 	}
 	return (1);
 }
