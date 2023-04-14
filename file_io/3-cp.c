84020596250dfdd252b7c06fb7f4f61f2ad4499b
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-14 12:20:59 -0700

    Copia el contenido de un archivo hacia otro archivo

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 		}
 		n2 = write(file_to, buffer, n1);
 		if (n2 < n1)
-		{	
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
