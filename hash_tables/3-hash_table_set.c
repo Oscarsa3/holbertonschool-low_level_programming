0d971e6494ac67b6c1854f80ac3becb9a6132cf2
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-14 14:53:45 -0700

    Agrega un nodo a un indice y si encuentra un valor en ese indice se agrega al inicio de la lista

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,22 +9,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
+	char *valor = strdup(value);
 
 	if (!ht | !key)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
-		ht->array[index]->value = strdup(value);
-		ht->array[index]->key = (char *)key;
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		ht->array[index]->value = valor;
+		ht->array[index]->key = strdup(key);
 		ht->array[index]->next = NULL;
 	}
 	else
 	{
+
 		ht->array[index]->next = malloc(sizeof(hash_node_t));
 		if (!ht->array[index]->next)
 			return (0);
-		ht->array[index]->next->value = strdup(value);
+		ht->array[index]->next->value = valor;
 		if (!ht->array[index]->next->value)
 			return (0);
 		ht->array[index]->next->key = (char *)key;
