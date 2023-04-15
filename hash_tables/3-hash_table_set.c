34b45bd2b1e65f512c4a120dd48e93261709edc0
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-14 17:48:26 -0700

    Agregando lineas para solo sobreescribir el valor si las key son iguales

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,59 +10,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
 	char *valor = strdup(value);
-	hash_node_t *ptr; 	
+	hash_node_t *ptr;
+	int igual;
 
 	if (!ht | !key)
-		return (0);
+	return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	ptr = ht->array[index];
-	if (!ptr)
-		ptr = new_nodo(NULL);
-	if (ptr->key)
+	if (ht->array[index] == NULL)
 	{
-		ht->array[index] = actualizar(ptr, key, valor);
-		return (1);
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		ht->array[index]->value = valor;
+		ht->array[index]->key = strdup(key);
+		ht->array[index]->next = NULL;
 	}
-	ptr->key = strdup(key);
-	ptr->value = valor;
-	ptr->next = NULL;
-	ht->array[index] = ptr;
+	else
+	{
+		igual = strcmp(ht->array[index]->value, valor);
+		if (igual == 0)
+			ht->array[index]->value = valor;
+			ptr = malloc(sizeof(hash_node_t));
+		if (!ptr)
+			return (0);
+		ptr->value = valor;
+		if (!ptr->value)
+			return (0);
+		ptr->key = (char *)key;
+		ptr->next = ht->array[index];
+		ht->array[index]->next = NULL;
+		ht->array[index] = ptr;
+	}
 	return (1);
-}
-hash_node_t *new_nodo(hash_node_t *nuevo)
-{
-	hash_node_t *nodo;
-
-	nodo = malloc(sizeof(hash_node_t));
-	if (!nodo)
-		return (NULL);
-	nodo->key = NULL;
-	nodo->value = NULL;
-	nodo->next = nuevo;
-	return (nodo);
-}
-hash_node_t *actualizar(hash_node_t *nodo, const char *key, char *valor)
-{
-	hash_node_t *tmp = nodo;
-
-	while (tmp)
-	{
-		if (!strcmp(key, tmp->key))
-		{
-			free(tmp->value);
-			tmp->value = valor;
-			return (nodo);
-		}
-		tmp = tmp->next;
-	}
-	tmp = nodo;
-	nodo = new_nodo(nodo);
-	if(!nodo)
-	{
-		nodo = tmp;
-		return (nodo);
-	}
-	nodo->key = strdup(key);
-	nodo->value = valor;
-	return (nodo);
 }
