afacdafcafe3d59103cde976f0a8e0f7f8387d2e
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-14 19:23:00 -0700

    Imprime el valor de la clave que se indique

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+  * hash_table_get - muestra el valor asignado a dicha clave
+  * @ht: tabla
+  * @key: clave
+  * Return: value
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	size_t index = key_index((unsigned char *)key, ht->size);
+
+	if (!ht | !key)
+		return (NULL);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (!strcmp(key, tmp->key))
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
