885a7712e88a00fda3a4ded1d5f49d9f59558d6b
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-16 19:01:36 -0700

    Añadiendo linea para arreglar problemas con colisiones

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,8 +37,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (!ptr)
 			return (0);
 		ptr->value = strdup(value);
-		if (!ptr->key)
-			return (0);
 		ptr->key = (char *)key;
 		ptr->next = ht->array[index];
 		ht->array[index]->next = NULL;
