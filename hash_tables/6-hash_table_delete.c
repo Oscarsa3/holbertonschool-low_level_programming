6fb3bca9b372892d0bcf07cea142c54b80679320
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-16 18:26:59 -0700

    Elimina una hash table y libera memoria

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,24 +1,30 @@
 #include "hash_tables.h"
 /**
-  *
+  * hash_table_delete - elemina una tabla hash
+  * @ht: tabla
+  * Return: nada
   */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp;
+	hash_node_t *tmp, *nodo, *h = ht;
 	size_t index = 0;
 
 	while (index < ht->size)
 	{
-		while (ht->array[index])
+		if (ht->array[index])
 		{
-			tmp = ht->array[index]->next;
-			free(ht->array[index]->key);
-			free(ht->array[index]->value);
-			free(ht->array[index]);
-			ht->array[index] = tmp;
+			nodo = ht->array[index;]
+			while (nodo)
+			{
+				tmp = nodo->next;
+				free(nodo->key);
+				free(nodo->value);
+				free(nodo);
+				nodo = tmp;
+			}
 		}
 		index++;
 	}
-	free(ht->array);
-	free(ht);
+	free(h->array);
+	free(h);
 }
