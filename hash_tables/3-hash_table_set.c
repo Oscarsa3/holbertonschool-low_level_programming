b503d93e7d34b1d07d1fa2aaf3aca63df839cb3a
Oscarsa3 <oscarorlandosalinassilvestre>
2023-04-16 21:40:44 -0700

    Solucion al problema de la colision

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,34 +11,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	size_t index;
 	hash_node_t *ptr, *tmp;
 
-	if (!ht | !key)
+	if (!ht | !key | (strcmp(key, "") == 0))
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	ptr = ht->array[index];
+	if (ptr && strcmp(key, ptr->key) == 0)
 	{
-		ht->array[index] = malloc(sizeof(hash_node_t));
-		ht->array[index]->value = strdup(value);
-		ht->array[index]->key = strdup(key);
-		ht->array[index]->next = NULL;
-	}
-	else
-	{
-		tmp = ht->array[index];
-		while (tmp)
-		{
-			if (strcmp(tmp->key, key) == 0)
-			{
-				tmp->value = strdup(value);
-				return (1);
-			}
-			tmp = tmp->next;
-		}
-		ptr = malloc(sizeof(hash_node_t));
+		free(ptr->value);
 		ptr->value = strdup(value);
-		ptr->key = strdup(key);
-		ptr->next = ht->array[index];
-		ht->array[index]->next = NULL;
-		ht->array[index] = ptr;
+		return (1);
 	}
+	tmp = malloc(sizeof(hash_node_t));
+	if (tmp == NULL)
+		return (0);
+	tmp->value = strdup(value);
+	tmp->key = strdup(key);
+	tmp->next = ht->array[index];
+	ht->array[index] = tmp;
 	return (1);
 }
